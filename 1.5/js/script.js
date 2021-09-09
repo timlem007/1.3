920a52212995b9214dc03a15c5c55c209a59c5e0
timlem007 <abd_temur@mail.ru>
2021-09-09 21:43:32 +0300

    ev

--- 1.5/js/script.js
+++ 1.5/js/script.js
@@ -1,34 +1,38 @@
-new Swiper ('.swiper-container', {
-	direction:'horizontal',
+new Swiper('.swiper-container', {
+	direction: 'horizontal',
 	autoHeight: true,
 	pagination: {
 		el: '.swiper-pagination',
-	   clickable: true,
-	    },
+		clickable: true,
+	},
 
-		 breakpoints: {
-			 320: {
-				slidesPerView: 'auto',
-      			spaceBetween: 16,
-				  slidersPerColumn: 1
-			 }
-		 },
+	breakpoints: {
+		320: {
+			slidesPerView: 'auto',
+			spaceBetween: 16,
+			slidersPerColumn: 1
+		}
+	},
 
 });
 
-const element = document.querySelector('.menu[hidden]');
+const menuLoaded = document.querySelector('.menu[hidden]');
 const styleElementUnlo = document.querySelector('.read-more__unloaded');
 const styleElementLo = document.querySelector('.read-more__loaded');
 const btn = document.querySelector("#btn");
-btn.onclick = function(){
-	element.style.maxHeight = "260px";
-	styleElementLo.style.display = "none";
-	styleElementUnlo.style.display = "block";
-  };
-  btn.unonclick = function(){
-	element.style.maxHeight = "170px";
-	styleElementLo.style.display = "block";
-	styleElementUnlo.style.display = "none";
-  };
-console.log("jfklskdm");
-document.querySelector(".title").interHTML = "njnrenvf";
+const menuUnloaded = document.querySelector(".menu_unloaded[hidden]");
+let isClicked = false;
+
+btn.addEventListener("click", () => {
+	isClicked = !isClicked;
+
+	if (isClicked) {
+		menuLoaded.style.maxHeight = "260px";
+		styleElementLo.style.display = "none";
+		styleElementUnlo.style.display = "block";
+	} else {
+		menuLoaded.style.maxHeight = "170px";
+		styleElementLo.style.display = "block";
+		styleElementUnlo.style.display = "none";
+	}
+       })
